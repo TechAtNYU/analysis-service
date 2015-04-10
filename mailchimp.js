d885d5655ccb3e2a861a58b8e8093bfa3b225f0d
Abhi Agarwal <me@abhiagarwal.com>
2015-04-10 14:05:25 -0400

    Updating with mailchimp analytics

---
+++ mailchimp.js
@@ -0,0 +1,34 @@
+var Q = require('q');
+var mcapi = require('./node_modules/mailchimp-api/mailchimp');
+mc = new mcapi.Mailchimp(process.env.MailChimpAPI);
+
+var GetAllReports = function() {
+	return Q.Promise(function(resolve, reject, notify) {
+		mc.campaigns.list({'status':'sent'}, function(campaignData) {
+			resolve(campaignData);
+		}, function(error){
+			reject(error);
+		});
+	});
+};
+
+var GetReportsById = function(id) {
+	return Q.Promise(function(resolve, reject, notify) {
+		mc.campaigns.list({campaign_id: id}, function(campaignData) {
+			var campaign = campaignData.data[0];
+			mc.reports.summary({cid:id}, function(reportData) {
+				resolve({'campaign': campaign, 'report': reportData});
+			}, function(error) {
+				reject(error);
+			});
+		});
+	});
+};
+
+GetReportsById('37bdafc2cd')
+	.then(function(data){
+		console.log(data.report);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
